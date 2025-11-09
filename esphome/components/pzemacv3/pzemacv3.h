08998f870a22e53dbdd6098e5c95d8522e14677a
LevKos <7747783+LevKos@users.noreply.github.com>
2025-11-09 16:58:42 +0200

    offline_skip_updates = 1;
          offline_skip_updates = 1;

--- esphome/components/pzemacv3/pzemacv3.h
+++ esphome/components/pzemacv3/pzemacv3.h
@@ -17,9 +17,10 @@ namespace esphome
 
     class PZEMACV3 : public PollingComponent, public modbus::ModbusDevice
     {
-      offline_skip_updates = 1;
 
     public:
+      offline_skip_updates = 1;
+
       void set_voltage_sensor(sensor::Sensor *voltage_sensor) { voltage_sensor_ = voltage_sensor; }
       void set_current_sensor(sensor::Sensor *current_sensor) { current_sensor_ = current_sensor; }
       void set_power_sensor(sensor::Sensor *power_sensor) { power_sensor_ = power_sensor; }

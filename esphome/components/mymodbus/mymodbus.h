02323dcdb9160fe30847010e1bf07d97160c1e01
LevKos <7747783+LevKos@users.noreply.github.com>
2025-12-04 20:15:56 +0200

    non oweride

--- esphome/components/mymodbus/mymodbus.h
+++ esphome/components/mymodbus/mymodbus.h
@@ -65,7 +65,7 @@ namespace esphome
       void set_address(uint8_t address) { address_ = address; }
       virtual void on_modbus_data(const std::vector<uint8_t> &data) = 0;
       virtual void on_modbus_error(uint8_t function_code, uint8_t exception_code) {}
-      virtual void on_modbus_offline(void) override;
+      virtual void on_modbus_offline(void) {}
       virtual void on_modbus_read_registers(uint8_t function_code, uint16_t start_address, uint16_t number_of_registers) {};
       virtual void on_modbus_write_registers(uint8_t function_code, const std::vector<uint8_t> &data) {};
       void send(uint8_t function, uint16_t start_address, uint16_t number_of_entities, uint8_t payload_len = 0,

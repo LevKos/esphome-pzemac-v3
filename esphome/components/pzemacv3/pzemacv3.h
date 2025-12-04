31886b2315d103b955584a41227eafc598bb3fb1
LevKos <7747783+LevKos@users.noreply.github.com>
2025-12-04 19:37:45 +0200

    include as related

--- esphome/components/pzemacv3/pzemacv3.h
+++ esphome/components/pzemacv3/pzemacv3.h
@@ -3,7 +3,7 @@
 #include "esphome/core/automation.h"
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
-#include "esphome/components/mymodbus/mymodbus.h"
+#include "e../mymodbus/mymodbus.h"
 
 #include <vector>
 
